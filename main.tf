0098748beb0d44363eaafc0ada2619829cbfd63c
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-10-21 16:49:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "Grafana" {
     authentication_providers = var.authentication_providers
     permission_type = var.permission_type
     Admin_permission = var.Admin_permission
-    tags = local.tags
+    #tags = local.tags
 }
